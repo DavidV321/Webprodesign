e5e28c20e49e32cc577b7ed69840920479f098f4
David Vychanek <vychanek@vychanek.cz>
2023-11-22 11:32:05 +0100

    treti comit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+(function($){
+    $(function(){
+       // sem přijde naše jQuery 
+    
+
+
+
+    /*......mobile navigation......*/
+ 
+//  $(".jq--nav-icon").click(function(){
+     
+//      $(".nav-backround").slideToggle(); 
+//      $(".mobile-nav-back").fadeToggle(); 
+//      $("nav ul").fadeToggle(); 
+       
+//     });   
+    
+   /*podmínky změna hamburgeru na cross a zpět */
+        
+//   $(".jq--image-hamburger").click(function(){
+      
+      
+//       if($(".jq--image-hamburger").attr("src") == "images/hamburgerMenu.png" )
+//      {     
+//     $( $(".jq--image-hamburger").attr("src","images/crossMenu.png"));
+//   }
+//      else
+//      {
+//    $( $(".jq--image-hamburger").attr("src","images/hamburgerMenu.png"));
+//      }
+      
+//     });
+   
+
+  
+      
+ 
+        
+    }); 
+ })(jQuery);
